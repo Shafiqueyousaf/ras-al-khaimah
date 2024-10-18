80649d78f0b1977506a3f13ca7574c338edbdb17
shafique yousaf <shafique.yousaf@arhamsoft.org>
2024-10-18 17:45:16 +0500

    added new folder

--- app/[lng]/(pages)/e-services/page.js
+++ app/[lng]/(pages)/e-services/page.js
@@ -2,7 +2,7 @@
 
 export default function Eservices(){
     return(
-        <section className="bg-membership-bg bg-no-repeat bg-cover min-h-60 max-sm:">
+        <section className="bg-membership-bg bg-no-repeat bg-cover min-h-60">
             
         </section>
         
--- app/something/Something.js
+++
@@ -1,3 +0,0 @@
-export default function Something (){
-    return <h1>Here we go</h1>
-}
